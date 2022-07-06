b1d78298298ad42f3f332745ff0e4bc8f277b2f1
chengkangzai <pycck@hotmail.com>
2022-07-07 00:46:39 +0800

    refactor

--- MyLand/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ MyLand/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -22,7 +22,7 @@ namespace MyLand.Areas.Identity.Pages.Account
         private readonly SignInManager<MyLandUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
 
-        public LoginModel(SignInManager<MyLandUser> signInManager, 
+        public LoginModel(SignInManager<MyLandUser> signInManager,
             ILogger<LoginModel> logger,
             UserManager<MyLandUser> userManager)
         {
@@ -62,7 +62,7 @@ namespace MyLand.Areas.Identity.Pages.Account
                 ModelState.AddModelError(string.Empty, ErrorMessage);
             }
 
-            returnUrl = returnUrl ?? Url.Content("~/");
+            returnUrl ??= Url.Content("~/");
 
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
@@ -74,7 +74,7 @@ namespace MyLand.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl = returnUrl ?? Url.Content("~/");
+            returnUrl ??= Url.Content("~/");
 
             if (ModelState.IsValid)
             {
