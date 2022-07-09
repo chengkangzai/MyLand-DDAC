7f2d944a9da38e441014d4b783449df353f566da
chengkangzai <pycck@hotmail.com>
2022-07-09 23:32:17 +0800

    use [Authorise] to ensure all route need login, refactor code, early return , remove all user login check

--- MyLand/Controllers/UsersController.cs
+++ MyLand/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,12 +14,14 @@ using MyLand.Areas.Identity.Data;
 
 namespace MyLand.Controllers
 {
+    [Authorize]
     public class UsersController : Controller
     {
         private readonly UserManager<MyLandUser> _userManager;
         private readonly SignInManager<MyLandUser> _signInManager;
+
         public UsersController(
-            UserManager<MyLandUser> userManager, 
+            UserManager<MyLandUser> userManager,
             SignInManager<MyLandUser> signInManager)
         {
             _userManager = userManager;
@@ -28,112 +31,107 @@ namespace MyLand.Controllers
         // GET: Users
         public async Task<IActionResult> Index()
         {
-            if (_signInManager.IsSignedIn(User))
+            var user = await _userManager.GetUserAsync(User);
+            if (user.Role != 1)
             {
-                var user = await _userManager.GetUserAsync(User);
-                if (user == null)
-                {
-                    return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-                }
-                if (user.Role == 1)
-                {
-                    var users = await _userManager.Users.ToListAsync();
-                    foreach (var item in users)
-                    {
-                        if (item.Role == 1) { user.Address = "Admin"; }
-                        else { user.Address = "User"; }
-                    }
-                    return View(users);
-                }
-                else
-                {
-                    return NotFound($"Only admin may delete");
-                }
+                return NotFound($"Only admin may delete");
             }
-            return NotFound($"User login required");
+            var users = await _userManager.Users.ToListAsync();
+            foreach (var item in users)
+            {
+                user.Address = item.Role == 1 ? "Admin" : "User";
+            }
+            return View(users);
         }
 
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(string username)
         {
-            if (_signInManager.IsSignedIn(User))
+            if (!_signInManager.IsSignedIn(User))
             {
-                var user = await _userManager.GetUserAsync(User);
-                if (user == null)
-                {
-                    return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-                }
-                if (user.Role == 1)
-                {
-                    if (username == null) { return NotFound(); }
-                    var targetUser = await _userManager.FindByNameAsync(username);
-                    if (targetUser == null) { return NotFound(); }
-                    await _userManager.DeleteAsync(targetUser);
-                    return RedirectToAction(nameof(Index));
-                }
-                else
-                {
-                    return NotFound($"Only admin may delete");
-                }
+                return NotFound($"User login required");
             }
-            return NotFound($"User login required");
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user.Role != 1)
+            {
+                return Unauthorized();
+            }
+            if (username == null)
+            {
+                return NotFound();
+            }
+            var targetUser = await _userManager.FindByNameAsync(username);
+            if (targetUser == null)
+            {
+                return NotFound();
+            }
+            await _userManager.DeleteAsync(targetUser);
+            return RedirectToAction(nameof(Index));
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> MakeUser(string username)
         {
-            if (_signInManager.IsSignedIn(User))
+            if (!_signInManager.IsSignedIn(User))
             {
-                var user = await _userManager.GetUserAsync(User);
-                if (user == null)
-                {
-                    return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-                }
-                if (user.Role == 1)
-                {
-                    if (username == null) { return NotFound(); }
-                    var targetUser = await _userManager.FindByNameAsync(username);
-                    if (targetUser == null) { return NotFound(); }
-                    targetUser.Role = 0;
-                    await _userManager.UpdateAsync(targetUser);
-                    return RedirectToAction(nameof(Index));
-                }
-                else
-                {
-                    return NotFound($"Only admin may change role");
-                }
+                return NotFound($"User login required");
             }
-            return NotFound($"User login required");
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+            if (user.Role != 1)
+            {
+                return NotFound($"Only admin may change role");
+            }
+            if (username == null)
+            {
+                return NotFound();
+            }
+            var targetUser = await _userManager.FindByNameAsync(username);
+            if (targetUser == null)
+            {
+                return NotFound();
+            }
+            targetUser.Role = 0;
+            await _userManager.UpdateAsync(targetUser);
+            return RedirectToAction(nameof(Index));
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> MakeAdmin(string username)
         {
-            if (_signInManager.IsSignedIn(User))
+            if (!_signInManager.IsSignedIn(User))
             {
-                var user = await _userManager.GetUserAsync(User);
-                if (user == null)
-                {
-                    return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-                }
-                if (user.Role == 1)
-                {
-                    if (username == null) { return NotFound(); }
-                    var targetUser = await _userManager.FindByNameAsync(username);
-                    if (targetUser == null) { return NotFound(); }
-                    targetUser.Role = 1;
-                    await _userManager.UpdateAsync(targetUser);
-                    return RedirectToAction(nameof(Index));
-                }
-                else
-                {
-                    return NotFound($"Only admin may change role");
-                }
+                return NotFound($"User login required");
             }
-            return NotFound($"User login required");
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+            if (user.Role != 1)
+            {
+                return NotFound($"Only admin may change role");
+            }
+            
+            if (username == null)
+            {
+                return NotFound();
+            }
+            var targetUser = await _userManager.FindByNameAsync(username);
+            if (targetUser == null)
+            {
+                return NotFound();
+            }
+            targetUser.Role = 1;
+            await _userManager.UpdateAsync(targetUser);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
