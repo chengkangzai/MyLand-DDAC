3b323a488c738b1255b69539544d5c8ba2622af5
chengkangzai <pycck@hotmail.com>
2022-07-13 23:43:21 +0800

    introduce aws helper

---
+++ MyLand/Services/AWSHelper.cs
@@ -0,0 +1,36 @@
+﻿using Amazon.S3;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MyLand.Services
+{
+    public class AWSHelper
+    {
+        public static string getBucketName()
+        {
+            return "myland-images";
+        }
+
+        public static string getBucketUrl()
+        {
+            return "https://s3.amazonaws.com/myland-images/";
+        }
+
+        public static AmazonS3Client getAmazonS3Client()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            IConfiguration configuration = builder.Build();
+
+            var accessKey = configuration["AWSCredential:key1"];
+            var secretKey = configuration["AWSCredential:key2"];
+            var tokenKey = configuration["AWSCredential:key3"];
+
+
+            return new AmazonS3Client(accessKey, secretKey, tokenKey, Amazon.RegionEndpoint.USEast1);
+        }
+    }
+}
